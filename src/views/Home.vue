4595bcf224d5ce3da4a909fb0ac7909b444da7a1
John Heinrich Austria <heinrichaustria@gmail.com>
2020-07-01 13:55:59 +0800

    Load and display tweets from JSON file

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,11 @@
     <NavBar />
     <main class="container">
       <ComposeTweet />
+      <div>
+        <div v-for="tweet in this.tweets" :key="tweet.id.toString()">
+          <Tweet v-bind:tweet="{ user, ...tweet }" />
+        </div>
+      </div>
     </main>
   </div>
 </template>
@@ -11,12 +16,25 @@
 // @ is an alias to /src
 import NavBar from '@/components/NavBar.vue';
 import ComposeTweet from '@/components/ComposeTweet.vue';
+import Tweet from '@/components/Tweet.vue';
+
+import data from '../data/tweets.json';
 
 export default {
   name: 'Home',
   components: {
     NavBar,
     ComposeTweet,
+    Tweet,
+  },
+  data() {
+    return {
+      user: {
+        name: 'John Doe',
+        username: 'jdoe',
+      },
+      tweets: data.tweets,
+    };
   },
 };
 </script>
@@ -29,5 +47,9 @@ export default {
   margin-right: auto;
   padding-left: 56px;
   padding-right: 56px;
+
+  @media screen and (max-width: 992px) {
+    padding: 16px;
+  }
 }
 </style>
